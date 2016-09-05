37ca0ea0b54edef0a592e22b8a727a840fef006b
widonwaker <hey.lol@live.it>
2016-09-05 14:39:01 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal.init("852b5fe5-9d76-49e5-a804-1650a055b2af",
+                                 {googleProjectNumber: "790260643795"},
+                                 notificationOpenedCallback);
+  
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
+}, false);
---
+++ js/load-json.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$(document).bind('deviceready', function(){
+		//Phonegap ready
+		onDeviceReady();
+	});   
+
+	var output = $('.swiper-wrapper');
+	
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false,
+		observer: true
+    });
+
+	$.ajax({
+		url: 'http://gestisciapp.it/sscg/db_to_app_home.php',
+		dataType: 'jsonp',
+		jsonp: 'jsoncallback',
+		timeout: 5000,
+		success: function(data, status){
+			$.each(data, function(i,item){ 
+				var landmark = '<div class="swiper-slide"><img src="http://gestisciapp.it/sscg/images/'+item.img+'" style="width:100%;"></img></div>';			
+				output.append(landmark);
+
+			}); 
+			
+		},
+		error: function(){
+			$( "#popupDialog" ).popup( "open" )
+		   //output.text('Non sei connesso ad internet! Abilita una connessione dati e riavvia l\'app!');
+		}
+	});
+	
+});
