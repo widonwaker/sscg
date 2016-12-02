6ebb4a01889812e2f4dc838574b25678b87910bb
widonwaker <hey.lol@live.it>
2016-12-02 13:25:34 +0100

    Update load-json.js

--- js/load-json.js
+++ js/load-json.js
@@ -4,19 +4,23 @@ $(document).ready(function(){
 		onDeviceReady();
 	});   
 
-	var output = $('.swiper-wrapper');
+	var output = $('.swipe-wrap');
 	
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        paginationClickable: true,
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false,
-		observer: true
-    });
+var element = document.getElementById('mySwipe'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next');
+
+window.mySwipe = new Swipe(element, {
+  startSlide: 0,
+  auto: 3000,
+  draggable: true,
+  autoRestart: false,
+  continuous: true,
+  disableScroll: true,
+  stopPropagation: true,
+  callback: function(index, element) {},
+  transitionEnd: function(index, element) {}
+});
 
 	$.ajax({
 		url: 'http://gestisciapp.it/sscg/db_to_app_home.php',
@@ -25,7 +29,7 @@ $(document).ready(function(){
 		timeout: 5000,
 		success: function(data, status){
 			$.each(data, function(i,item){ 
-				var landmark = '<div class="swiper-slide"><img src="http://gestisciapp.it/sscg/images/'+item.img+'" style="width:100%;"></img></div>';			
+				var landmark = '<div><img src="http://gestisciapp.it/sscg/images/'+item.img+'" style="width:100%;"></img></div>';			
 				output.append(landmark);
 
 			}); 
