e22dc45ba4e1c368c9b0a84e9b9c1e4012f5dc1f
widonwaker <hey.lol@live.it>
2016-12-05 15:02:25 +0100

    Update load-json.js

--- js/load-json.js
+++ js/load-json.js
@@ -1,24 +1,16 @@
-$(document).ready(function(){
-	$(document).bind('deviceready', function(){
-		//Phonegap ready
-		onDeviceReady();
-	});   
-
-
-	var output = $('.swipe-wrap');
-	
-	$.ajax({
+var output = $('.swipe-wrap');
+$.ajax({
 		url: 'http://gestisciapp.it/sscg/db_to_app_home.php',
 		dataType: 'jsonp',
 		jsonp: 'jsoncallback',
-		timeout: 5000,
+		timeout: 2000,
 		success: function(data, status){
 			$.each(data, function(i,item){ 
 				var landmark = '<div><img src="http://gestisciapp.it/sscg/images/'+item.img+'" style="width:100%;"></img></div>';			
 				output.append(landmark);
+
 			}); 
-			
-    var element = document.getElementById('mySwipe'),
+			var element = document.getElementById('mySwipe'),
     prevBtn = document.getElementById('prev'),
     nextBtn = document.getElementById('next');
 
@@ -33,12 +25,10 @@ window.mySwipe = new Swipe(element, {
   callback: function(index, element) {},
   transitionEnd: function(index, element) {}
 });
-			
+
 		},
 		error: function(){
 			$( "#popupDialog" ).popup( "open" )
 		   //output.text('Non sei connesso ad internet! Abilita una connessione dati e riavvia l\'app!');
 		}
 	});
-	
-});
