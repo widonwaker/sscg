4cce1577c113a193c58ef54dfc73ad1132baa73f
widonwaker <hey.lol@live.it>
2016-12-05 13:56:45 +0100

    Update load-json.js

--- js/load-json.js
+++ js/load-json.js
@@ -30,6 +30,22 @@ $(document).ready(function(){
 
 			}); 
 			
+    var element = document.getElementById('mySwipe'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next');
+
+window.mySwipe = new Swipe(element, {
+  startSlide: 0,
+  auto: 3000,
+  draggable: true,
+  autoRestart: true,
+  continuous: true,
+  disableScroll: true,
+  stopPropagation: true,
+  callback: function(index, element) {},
+  transitionEnd: function(index, element) {}
+});
+			
 		},
 		error: function(){
 			$( "#popupDialog" ).popup( "open" )
