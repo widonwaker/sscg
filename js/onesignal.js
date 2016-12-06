dd23afd254c930e14f808f1aab80d4ed6bb1edfd
widonwaker <hey.lol@live.it>
2016-12-06 20:05:05 +0100

    Update onesignal.js

--- js/onesignal.js
+++ js/onesignal.js
@@ -2,7 +2,7 @@
 // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
-document.addEventListener('deviceready', function () {
+/*document.addEventListener('deviceready', function () {
   // Enable to debug issues.
   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
@@ -12,6 +12,27 @@ document.addEventListener('deviceready', function () {
 
   window.plugins.OneSignal
     .startInit("8dcd7084-39b6-4e2c-af5a-58caf7d76d43", "790260643795")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Sync hashed email if you have a login system or collect it.
+  //   Will be used to reach the user at the most optimal time of day.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false); */
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("8dcd7084-39b6-4e2c-af5a-58caf7d76d43")
     .handleNotificationOpened(notificationOpenedCallback)
     .endInit();
   
